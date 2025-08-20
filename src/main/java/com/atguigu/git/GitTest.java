17876fc490c10dfa90b390036eb64219ecdd50a0
AC <690124552@qq.com>
2025-08-20 10:05:56 +0800

    hotfix commit222

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello jjj");
         System.out.println("hello jasdaj");
-        System.out.println("hello dasadaddadwaaj");
+        System.out.println("hello dasatestwaaj");
     }
 }
