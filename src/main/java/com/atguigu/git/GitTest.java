2d6fad905d4769586f04d7d93156ba519ed5c39e
AC <690124552@qq.com>
2025-08-20 09:59:38 +0800

    3 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello jjj");
         System.out.println("hello jasdaj");
+        System.out.println("hello dasadaj");
     }
 }
