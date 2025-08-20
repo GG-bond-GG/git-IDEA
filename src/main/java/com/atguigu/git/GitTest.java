c78f29cda48bfdc18f2d32477845714e7225596e
GG-bond-GG <150571932+GG-bond-GG@users.noreply.github.com>
2025-08-20 10:47:16 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,8 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello jjj");
-        System.out.println("hello jasdaj");
-        System.out.println("hello dasadaddadwaaj");
+
         System.out.println("hello test");
         System.out.println("push test");
     }
